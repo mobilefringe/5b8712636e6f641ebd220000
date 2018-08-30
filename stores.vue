1b5d82dad3556090cdec5246f87807bffb4bab46
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-30 17:29:14 -0400

    Peninsula 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -54,7 +54,14 @@
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
-                                			<img class="store_img" :src="store.image_url" alt="">
+                                			<!--<img class="store_img" :src="store.image_url" alt="">-->
+                                			<img v-if="!store.no_store_logo" class="store_img" :src="store.store_front_url_abs" alt="">
+                                    <div v-else class="no_logo_container">
+                                        <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
+                                        <div class="no_logo_text">
+                                            <div class="store_text"><h4>{{ store.name }}</h4></div>
+                                        </div>
+                                    </div>
                                 			<div class="store_tag" v-if="store.total_published_promos">
             									<div class="store_tag_text">Promotion</div>
             								</div>
@@ -183,9 +190,9 @@
                         if(!_.includes(value.categories, vm.dineFilter)) {
                             if (_.includes(value.image_url, 'missing')) {
                                value.no_store_logo = true;
-        } else {
-          value.no_store_logo = false;
-        }
+                            } else {
+                              value.no_store_logo = false;
+                            }
                             store_list.push(value);
                         }
                     });
