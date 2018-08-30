a93430e6ccdae9b6e346890516d2feba821ab794
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-30 17:39:45 -0400

    Peninsula 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -112,7 +112,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                     },
                     {
                         name: "Dine",
-                        href: "/Dine"
+                        href: "/dine"
                     },
                     {
                         name: "Location",
