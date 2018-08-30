e7f0d0d78ffee298a9b14e51276ce4a4bf430934
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-30 17:38:53 -0400

    Peninsula 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                     },
                     {
                         name: "Events",
-                        href: "/events"
+                        href: "/events-and-promotions"
                     },
             
                     {
