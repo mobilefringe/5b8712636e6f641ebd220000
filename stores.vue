c74575f085f7ce583fb7366479462444764fc2a4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-31 12:28:34 -0400

    Peninsula 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -125,12 +125,16 @@
             },
             created (){
                 this.loadData().then(response => {
-                    // var temp_repo = this.findRepoByName('Directory Banner').images;
-                    // if(temp_repo != null) {
-                    //     this.storeBanner = temp_repo[0];
-                    // } else {
-                    //     this.storeBanner = "http://via.placeholder.com/1920x400/4f6726/4f6726";
-                    // }
+                    var temp_repo = this.findRepoByName('Directory Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       this.pageBanner = temp_repo[0];
+                    }
+                    else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                        }
+                    }
                     
                     this.dataLoaded = true;
                     
