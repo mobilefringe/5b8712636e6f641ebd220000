ce25b87b848ae252f262ee5453c2e4a1f61d7066
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-08-31 10:41:08 -0400

    Peninsula 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -56,6 +56,7 @@
                             	        <router-link :to="'/stores/'+ store.slug">
                                 			<!--<img class="store_img" :src="store.image_url" alt="">-->
                                 			<div v-if="!store.no_store_logo">
+                                			<img src="//codecloud.cdn.speedyrails.net/sites/5b8712636e6f641ebd220000/image/png/1533845184449/transparent_logo.png"
                                 			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
                                 			</div>
                                 			
