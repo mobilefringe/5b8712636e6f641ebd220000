717231a5c90af463019c1aada33b809fdc7d8fd8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-04 14:29:57 -0400

    Peninsula 2.0 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -80,7 +80,7 @@
                     
                     if(response && response[1]){
                         this.main = response[1].data;
-                        if(response[1].data && response[1].data.subpages)
+                        if(response[1].data && response[1].data.subpages && response[1].data.subpages[0])
                         this.leasingInfo = response[1].data.subpages[0]
                     }
                     this.dataLoaded = true;
