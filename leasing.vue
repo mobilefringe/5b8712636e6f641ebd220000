6531259b5256d2da6274d510cd4cf8b1e9fdfd57
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-04 14:29:50 -0400

    Peninsula 2.0 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -79,8 +79,9 @@
                     }
                     
                     if(response && response[1]){
-                    this.main = response[1].data;
-                    this.leasingInfo = response[1].data.subpages[0]
+                        this.main = response[1].data;
+                        if(response[1].data && response[1].data.subpages)
+                        this.leasingInfo = response[1].data.subpages[0]
                     }
                     this.dataLoaded = true;
                 });
