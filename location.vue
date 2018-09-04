89418457bb95ba438e25b1ea5da3a74017641a9d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-04 14:47:15 -0400

    Peninsula 2.0 1.0  /location.vue

---
+++ location.vue
@@ -0,0 +1,100 @@
+<template>
+    <div> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_page_header">
+                    <div class="main_container position_relative">
+                        <h2>Location</h2>
+                    </div>
+                </div>
+                <div class="main_container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <breadcrumb></breadcrumb>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div v-if="main" v-html="main.body"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="location_map">
+                    <google-map :property="property" :zoom="16"></google-map>
+                </div>
+                <div class="main_container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div v-if="address" v-html="address.body"></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div v-if="directions" v-html="directions.body"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="location_image_container">
+                    <div class="location_image" v-if="pageImages" v-for="item in pageImages">
+                        <img :src="item.image_url" :alt="item.name" class="img_max" />   
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+	define(["Vue", "vuex", "vue!google_map"], function(Vue, Vuex, google_map ) {
+		return Vue.component("location-component", {
+            template: template, // the variable template will be injected
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    main: null,
+                    address: null,
+                    directions: null,
+                    pageImages: null
+                }
+            },
+            created() {
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Location Images');
+                    if(temp_repo) {
+                        this.pageImages = temp_repo.images;
+                    }
+                    if(response && response[1]){
+                        this.main = response[1].data;
+                        if(response[1].data && response[1].data.subpages && response[1].data.subpages[0]){
+                            this.leasingInfo = response[1].data.subpages[0]
+                        }
+                    }
+                    this.main = response[1].data;
+                    this.address = response[1].data.subpages[0]
+                    this.directions = response[1].data.subpages[1]
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'repos',
+                    'findRepoByName'
+                ]),
+                getPropertyAddress() {
+                    return this.property.name + ' ' + this.property.address1 + ' ' + this.property.city + ' ' + this.property.country + ' ' +this.property.province_state + ' ' + this.property.province_state
+                }
+            },
+            methods: {
+                loadData: async function () {
+                    this.property.mm_host = this.property.mm_host.replace("http:", "");
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/cerritos-location.json"})]);
+                        return results;
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                }
+            }
+        });
+	});
+</script>
