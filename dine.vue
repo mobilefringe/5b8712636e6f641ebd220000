47e2a65ce1a9b42edc9e96fce782db1428a84e0c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-05 12:02:51 -0400

    Peninsula 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -184,7 +184,7 @@
                 },
                 dropDownCats() {
                     var vm = this;
-                    var dining_cat =  _.find(this.processedCategories, function(o) { return o.name == "Dining"});
+                    var dining_cat =  _.find(this.processedCategories, function(o) { return o.id == "Dining"});
                     var subcategories = [];
                     if (dining_cat !== null && dining_cat !== undefined) {
                        subcategories = vm.findSubcategoriesByParentID(dining_cat.id);
