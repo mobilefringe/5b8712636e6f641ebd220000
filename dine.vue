755a2dbbc562ac7d6114f1ab1de36dd8d79f46c2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-05 12:02:08 -0400

    Peninsula 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -54,7 +54,18 @@
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
-                                			<img class="store_img" :src="store.image_url" alt="">
+                                			<!--<img class="store_img" :src="store.image_url" alt="">-->
+                                			<div v-if="!store.no_store_logo">
+                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png">
+                                			    <img  class="store_img" :src="store.store_front_url_abs" alt="">
+                                			</div>
+                                			
+                                            <div v-else class="no_logo_container">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
+                                                <div class="no_logo_text">
+                                                    <div class="store_text"><h4>{{ store.name }}</h4></div>
+                                                </div>
+                                            </div>
                                 			<div class="store_tag" v-if="store.total_published_promos">
             									<div class="store_tag_text">Promotion</div>
             								</div>
@@ -65,7 +76,7 @@
             									<div class="store_tag_text">New Store</div>
             								</div>
             								<div class="store_details">
-            								    <div class="store_text"><h3>{{ store.name }}</h3></div>    
+            								    <div class="store_text"><h4>{{ store.name }}</h4></div>    
             								</div>
                                 		</router-link>
                             	    </div>
