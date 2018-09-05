e629a0c1c3c4e26db8b562867a020d03ab9a84cb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-05 12:50:09 -0400

    Peninsula 2.0 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -21,7 +21,7 @@
                     </div>
                     <div class="row">
                         <div class="col-md-4">
-                            <div class="margin_60 padding_60" v-html="contactInfo.body"></div>    
+                            <div v-if="contactInfo" class="margin_60 padding_60" v-html="contactInfo.body"></div>    
                         </div>
                         <div class="col-md-8">
                             <transition name="fadeIn">
