1370342783e7dfe3ade7b2f82a262ab26f047ccf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-05 14:42:49 -0400

    Peninsula 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -240,7 +240,7 @@
             methods: {
                 loadData: async function() {
                     try {
-                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://peninsula.mallmaverick.com/api/v4/"+this.property.subdomain+"/social.json"})]);
+                        let results = await Promise.all([this.$store.dispatch("getData", "banners"), this.$store.dispatch("getData","promotions"), this.$store.dispatch("getData", "events"), this.$store.dispatch('LOAD_PAGE_DATA', {url: "https://home.mallmaverick.com/api/v4/"+this.property.subdomain+"/social.json"})]);
                         return results;
                     } catch(e) {
                         console.log("Error loading data: " + e.message);    
