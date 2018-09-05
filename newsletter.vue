15b04dae4d701cb6a9c4e83e5973a78049979245
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-05 13:11:10 -0400

    Peninsula 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -23,13 +23,13 @@
                                     </div>
                                     <div class="col-sm-6" >
                                         <label for="fieldfihudi" class="accessibility">Last Name</label>
-                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-ulikhh-ulikhh" type="text" placeholder="Last Name">
+                                        <input v-model="form_data.last_name" required class="margin_20 form-control" id="fieldfihudi" name="cm-f-fihudi" type="text" placeholder="Last Name">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <label for="newsletter_email" class="accessibility">Email</label>
-                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-vjklyu-vjklyu" type="email" placeholder="Email" id="newsletter_email">
+                                        <input v-model="form_data.email" required class="margin_20 form-control" name="cm-ulikhh-ulikhh" type="email" placeholder="Email" id="newsletter_email">
                                     </div>
                                     <div class="col-sm-6">
                                         <label for="fieldfihudd" class="accessibility">Phone Number</label>
