114641a00b0c4d1c4e10c56218b703ede581c00b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 13:12:19 -0400

    Peninsula 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -92,6 +92,17 @@
 					if (this.currentPromo === null || this.currentPromo === undefined) {
 						this.$router.replace({ path: '/promotions' });
 					}
+					else {
+					    if (this.currentPromo.promotionable_type === "Store"){
+                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+                                this.currentPromo.image_url = this.currentPromo.store.store_front_url_abs; 
+                            }
+                        } else {
+                            if  (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+                                this.currentPromo.image_url = "//codecloud.cdn.speedyrails.net/sites/5b8712636e6f641ebd220000/image/png/1529532181000/promoplaceholder2@2x.png";    
+                            }
+                        }
+					}
 					this.$breadcrumbs[2].meta.breadcrumb = this.currentPromo.name
 					this.dataLoaded = true;
 				}, error => {
