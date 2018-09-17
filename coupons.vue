f80fe05b0abda6b9e014980a96fbdfdab43eb8e2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 12:58:06 -0400

    Peninsula 2.0 1.0  /coupons.vue

--- coupons.vue
+++ coupons.vue
@@ -85,6 +85,16 @@
                     // Cookies.remove('coupon_ids');
                     // this.cachedCoupons();
                     // this.selectedCoupons = Cookies.get('coupon_ids');
+                    var temp_repo = this.findRepoByName('Coupons Banner');
+                    if(temp_repo !== null && temp_repo !== undefined) {
+                       temp_repo = temp_repo.images;
+                       this.pageBanner = temp_repo[0];
+                    }
+                    else {
+                        this.pageBanner = {
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                        }
+                    }
                     this.initalizeCouponsfromCookies();
                     this.dataLoaded = true;
                 });
