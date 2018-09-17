54e340037b12b2a88d62619f827397971a0ecad1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 16:49:44 -0400

    Peninsula 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -127,7 +127,7 @@
                 loadData: async function () {
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+Site.subdomain+"-newsletter.json"}), this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url: this.property.mm_host + "/pages/"+Site.subdomain+"-newsletter.json"})]);
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
