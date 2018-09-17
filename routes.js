5d1bba95aa287ad879738aa021fa56be3af4099a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-09-17 17:09:04 -0400

    Peninsula 2.0 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -207,7 +207,46 @@ define([], function () {
                             name: 'map'
                         }
                     ]
-                }
+                },
+                {
+                    path: '/online_offers',
+                    component: view('default'),
+                    meta: {
+                        breadcrumb: 'Coupons',
+                    },
+                    children: [
+                        {
+                            path: '',
+                            component: view('coupons'),
+                            
+                            name: 'coupons'
+                        },
+                        {
+                            path: '/online_offers/my-basket',
+                            component: view('default'),
+                            children: [
+                                {
+                                    path: '',
+                                    component: view('coupon_basket'),
+                                    meta: {
+                                        breadcrumb: 'My Basket',
+                                    },
+                                    name: 'couponBasket',
+                                    props: true
+                                }
+                            ]
+                        },
+                        {
+                            path: ':id',
+                            component: view('coupons_details'),
+                            meta: {
+                                breadcrumb: 'Coupon Details',
+                            },
+                            name: 'couponDetails',
+                            props: true
+                        }
+                    ]
+                },
             ]
         },
         {
