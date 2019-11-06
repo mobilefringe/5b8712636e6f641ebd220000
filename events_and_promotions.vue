2c63e4d948e50d4db850764f210f9a27ee0d4b5d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-05 23:03:58 -0500

    Peninsula 2.0 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -63,9 +63,9 @@
                                         </p>
                                         <h4 class="event_name">{{ item.name }}</h4>
                                         <p class="event_dates"><span v-if="isMultiDay(item)">{{ item.start_date | moment("MMMM D", timezone)}} - {{ item.end_date | moment("MMMM D", timezone)}}</span><span v-else>{{ item.start_date | moment("MMMM D", timezone)}}</span></p>
-                                        <div class="event_desc" v-html="event.description_short"></div>
+                                        <div class="event_desc" v-html="item.description_short"></div>
                                         <router-link :to="{ name: 'promotionDetails', params: { id: item.slug, banner: pageBanner }}">
-                                            <div class="animated_btn event_link">View Proomotion Details <i class="fas fa-angle-double-right"></i></div>
+                                            <div class="animated_btn event_link">Promotion Details <i class="fas fa-angle-double-right"></i></div>
                                         </router-link>
                                         <hr class="event_seperator">
                                     </div>
