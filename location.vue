f236673992dff01801887e270ac27b51880c3a68
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-05 23:20:25 -0500

    Peninsula 2.0 1.0  /location.vue

--- location.vue
+++ location.vue
@@ -99,6 +99,12 @@
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
                     }
+                },
+                propertyAddress() {
+                    var address = this.property.name + "+" + this.property.address1 + "+" + this.property.city + "+" + this.property.province_state + "+" + this.property.country + this.property.postal_code
+                    var key ="AIzaSyCukCjH3fsuDYBdI44hZKL43m60jEToJjY"
+                    var src = "https://www.google.com/maps/embed/v1/place?q=" + address + "&key="+ key
+                    return src
                 }
             }
         });
