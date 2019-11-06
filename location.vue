f585707b7c4771422be37abe01e42071583503f1
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-05 23:19:45 -0500

    Peninsula 2.0 1.0  /location.vue

--- location.vue
+++ location.vue
@@ -3,7 +3,7 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-                <div class="inside_page_header" v-if="pageBanner" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(' + pageBanner.image_url + ') center center' }">
+                <div class="inside_page_header" v-bind:style="{ background: 'linear-gradient(0deg, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(' + pageBanner.image_url + ') center center rgb(49, 55, 113)' }">
                     <div class="main_container position_relative">
                         <h2>Location</h2>
                     </div>
@@ -20,8 +20,10 @@
                         </div>
                     </div>
                 </div>
-                <div class="location_map">
-                    <google-map :property="property" :zoom="16"></google-map>
+                <div class="main_container">
+                    <div class="location_map">
+                        <iframe :src="propertyAddress()" width="100%" height="490" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
                 </div>
                 <div class="main_container">
                     <div class="row">
@@ -44,28 +46,27 @@
 </template>
 
 <script>
-	define(["Vue", "vuex", "vue!google_map","json!site.json"], function(Vue, Vuex, google_map, Site) {
+	define(["Vue", "vuex", "vue!google_map"], function(Vue, Vuex, google_map ) {
 		return Vue.component("location-component", {
             template: template, // the variable template will be injected
             data: function () {
                 return {
                     dataLoaded: false,
+                    pageBanner: null,
                     main: null,
                     address: null,
                     directions: null,
                     pageImages: null
                 }
             },
-            created() {
+             created() {
                 this.loadData().then(response => {
-                    var repo = this.findRepoByName('Location Banner');
-                    if(repo !== null && repo !== undefined) {
-                       repo = repo.images;
-                       this.pageBanner = repo[0];
-                    }
-                    else {
+                    var repo = this.findRepoByName('Location Banner').images;
+                    if(repo != null) {
+                        this.pageBanner = repo[0];
+                    } else {
                         this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71eb886e6f6450013c0000/image/jpeg/1529532304000/insidebanner2.jpg"
+                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5d7017836e6f641aaf4f0000/image/jpeg/1529532304000/insidebanner2.jpg"
                         }
                     }
                     
@@ -73,16 +74,9 @@
                     if(temp_repo) {
                         this.pageImages = temp_repo.images;
                     }
-                    if(response && response[1]){
-                        this.main = response[1].data;
-                        if(response[1].data && response[1].data.subpages){
-                           this.address = response[1].data.subpages[0];
-                           this.directions = response[1].data.subpages[1]
-                        }
-                    }
-                    // this.main = response[1].data;
-                    // this.address = response[1].data.subpages[0]
-                    // this.directions = response[1].data.subpages[1]
+                    this.main = response[1].data;
+                    this.address = response[1].data.subpages[0]
+                    this.directions = response[1].data.subpages[1]
                     this.dataLoaded = true;
                 });
             },
