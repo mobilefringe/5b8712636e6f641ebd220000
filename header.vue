2be6f7bf9b10310ecb2e6f4731c896efb917e6b8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-11-22 12:15:24 -0500

    Peninsula 2.0 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -6,8 +6,7 @@
                     <div class="col-md-12">
                         <div class="social_icon_container">
     					    <span class="social_icon" v-for="item in social_media">
-                                <a :href="item.url" target="_blank">
-                                    <p class="accessibility">{{item.name}}</p>
+                                <a :href="item.url" target="_blank" :aria-label="'Follow Us on ' + item.name">
                                     <i :class="item.iconClass" aria-hidden="true"></i>
                                 </a>
                             </span>
